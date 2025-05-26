a730745fc84fbce60ef37a9f0b4cb58240161158
Jakob Jilg <jakobjilg96@web.de>
2025-05-26 21:50:57 +0200

    test

--- start2.sh
+++ start2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pkill -f jekyll
 bundle exec jekyll clean
-nohup bundle exec jekyll serve --livereload > ../start/jekyll.log 2>&1 & #the & is important to make nohup run in background, otherwise next line is not reached
+nohup bundle exec jekyll serve --livereload > ../x_log/jekyll.log 2>&1 & #the & is important to make nohup run in background, otherwise next line is not reached
 echo "server should be running"
 echo
 sleep 5
